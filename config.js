d8a36367d73733d88207299d6397b3b5996e126b
Kevin Smith <kevin@kevinsmith.io>
2017-01-03 16:24:16 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.host = process.env.HOST || "[URI for DocumentDB endpoint]";
 config.authKey = process.env.AUTH_KEY || "[Master key for DocumentDB]";
-config.databaseId = "ToDoList";
-config.collectionId = "Items";
+config.databaseId = "todo-db";
+config.collectionId = "todo";
 
 module.exports = config;
